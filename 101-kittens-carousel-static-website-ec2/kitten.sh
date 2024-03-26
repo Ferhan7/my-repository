afb738364f9987287bc10eabcc9c4341bb351d44
ferhan7 <ferhankulahci@gmail.com>
2024-03-26 09:31:55 -0500

    Kitten-test

--- 101-kittens-carousel-static-website-ec2/kitten.sh
+++ 101-kittens-carousel-static-website-ec2/kitten.sh
@@ -1,13 +1,19 @@
 #! /bin/bash
+
 yum update -y
 yum install httpd -y
-FOLDER="https://raw.githubusercontent.com/Ferhan7/my-repository/main/101-kittens-carousel-static-website-ec2/static-web/"
+
 
 cd /var/www/html
+FOLDER="https://raw.githubusercontent.com/Ferhan7/my-repository/main/101-kittens-carousel-static-website-ec2/static-web/"
+
+
+
 wget ${FOLDER}/index.html
 wget ${FOLDER}/cat0.jpg
 wget ${FOLDER}/cat1.jpg
 wget ${FOLDER}/cat2.jpg
-wget ${FOLDER}/cat3.png
+
 systemctl start httpd
 systemctl enable httpd
+
